862675f957918489f8020f88cb555c7413f01f1b
SebastianJHM <53499619+SebastianJHM@users.noreply.github.com>
2019-10-29 23:12:20 -0500

    navbar

--- Front-End/WareHouse/src/app/app-routing.module.ts
+++ Front-End/WareHouse/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PickingLPComponent } from './picking-lp/picking-lp.component';
+import { AllocationComponent } from './allocation/allocation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pick', component: PickingLPComponent },
+  { path: 'alloc', component: AllocationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
